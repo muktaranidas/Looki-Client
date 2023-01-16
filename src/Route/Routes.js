5ae89a436a9b4bcfca0969d7aebebfe1b9cba8c3
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-01-16 03:17:39 -0800

    login register design done

--- src/Route/Routes.js
+++ src/Route/Routes.js
@@ -1,4 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
+import Login from "../components/Login/Login";
+import Registration from "../components/Registration/Registration";
 import Home from "../components/Shared/Home/Home";
 import Main from "../Layout/Main/Main";
 
@@ -10,6 +12,15 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+
+      {
+        path: "/users/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/users/register",
+        element: <Registration></Registration>,
       },
     ],
   },
