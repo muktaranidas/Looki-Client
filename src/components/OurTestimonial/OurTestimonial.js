b48b0011d54a7d8ce77cf4a3e4096f886262e940
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-01-18 23:50:53 -0800

    your gift done

--- src/components/OurTestimonial/OurTestimonial.js
+++ src/components/OurTestimonial/OurTestimonial.js
@@ -1,27 +1,32 @@
 import React from "react";
+import fatema from "../../assets/Customers/Fatema.PNG";
+import rana from "../../assets/Customers/Rana.jpg";
+import mukta from "../../assets/Customers/Mukta.jpg";
 
 const OurTestimonial = () => {
   return (
-    <section className=" bg-white ">
+    <section className=" bg-white  ">
       <div className="container flex flex-col items-center mx-auto mb-12 md:p-10 md:px-12">
         <h1 className="p-4 uppercase text-2xl  leading-none text-center">
           our testimonial
         </h1>
-        <h1 className=" uppercase text-2xl  leading-none text-center">*</h1>
+        <h1 className=" uppercase text-2xl  text-orange-400 leading-none text-center">
+          *
+        </h1>
         <h1 className=" uppercase text-4xl uppercase  font-semibold leading-none text-center">
           what's our customer say
         </h1>
       </div>
-      <div className="container flex flex-col items-center justify-center mx-auto lg:flex-row lg:flex-wrap lg:justify-evenly lg:px-10">
+      <div className="container  flex flex-col items-center justify-center mx-auto lg:flex-row lg:flex-wrap lg:justify-evenly lg:px-10">
         <div className="flex flex-col max-w-sm mx-4 mb-16 ">
           <div className="flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-violet-400 dark:text-gray-900">
             <img
-              src="https://source.unsplash.com/50x50/?portrait?1"
+              src={fatema}
               alt=""
               className="w-28 h-28 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700"
             />
-            <p className="text-xl font-semibold leading-tight">
-              Distinctio Animi
+            <p className="text-xl pt-4 font-semibold leading-tight">
+              Fatema Summenur
             </p>
           </div>
           <div className="rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
@@ -30,7 +35,7 @@ const OurTestimonial = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
                 fill="currentColor"
-                className="w-6 h-6 dark:text-violet-400"
+                className="w-6 h-6 text-orange-400 dark:text-violet-400"
               >
                 <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
                 <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
@@ -42,7 +47,7 @@ const OurTestimonial = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
                 fill="currentColor"
-                className="absolute right-0 w-8 h-8 dark:text-violet-400"
+                className="absolute right-0 w-6 h-6 text-orange-400 dark:text-violet-400"
               >
                 <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
                 <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
@@ -53,12 +58,12 @@ const OurTestimonial = () => {
         <div className="flex flex-col max-w-sm mx-4 mb-16 ">
           <div className="flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-violet-400 dark:text-gray-900">
             <img
-              src="https://source.unsplash.com/50x50/?portrait?1"
+              src={rana}
               alt=""
               className="w-28 h-28 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700"
             />
-            <p className="text-xl font-semibold leading-tight">
-              Distinctio Animi
+            <p className="text-xl pt-4 font-semibold leading-tight">
+              Masud Rana
             </p>
           </div>
           <div className="rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
@@ -67,7 +72,7 @@ const OurTestimonial = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
                 fill="currentColor"
-                className="w-6 h-6 dark:text-violet-400"
+                className="w-6 h-6 text-orange-400 dark:text-violet-400"
               >
                 <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
                 <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
@@ -79,7 +84,7 @@ const OurTestimonial = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
                 fill="currentColor"
-                className="absolute right-0 w-8 h-8 dark:text-violet-400"
+                className="absolute right-0 w-6 h-6 text-orange-400 dark:text-violet-400"
               >
                 <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
                 <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
@@ -90,12 +95,12 @@ const OurTestimonial = () => {
         <div className="flex flex-col max-w-sm mx-4 mb-16 ">
           <div className="flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-violet-400 dark:text-gray-900">
             <img
-              src="https://source.unsplash.com/50x50/?portrait?1"
+              src={mukta}
               alt=""
               className="w-28 h-28 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700"
             />
-            <p className="text-xl font-semibold leading-tight">
-              Distinctio Animi
+            <p className="text-xl pt-4 font-semibold leading-tight">
+              Mukta Thakur
             </p>
           </div>
           <div className="rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
@@ -104,7 +109,7 @@ const OurTestimonial = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
                 fill="currentColor"
-                className="w-6 h-6 dark:text-violet-400"
+                className="w-6 h-6 text-orange-400 dark:text-violet-400"
               >
                 <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
                 <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
@@ -116,7 +121,7 @@ const OurTestimonial = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 512 512"
                 fill="currentColor"
-                className="absolute right-0 w-8 h-8 dark:text-violet-400"
+                className="absolute right-0 w-6 h-6 text-orange-400 dark:text-violet-400"
               >
                 <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
                 <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
