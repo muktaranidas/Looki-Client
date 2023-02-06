66af0ebdf1f402d81d0c3c8ca0bb05501953fe6b
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-02-05 23:11:33 -0800

    done

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import image1 from "../../assets/Categories/1.jpg";
 import image2 from "../../assets/Categories/2.jpg";
 import image3 from "../../assets/Categories/3.jpg";
@@ -10,48 +10,19 @@ import image8 from "../../assets/Categories/8.jpg";
 import Category from "./Category";
 
 const Categories = () => {
-  const categories = [
-    {
-      id: 1,
-      name: "Whitening & Brightening",
-      image: image1,
-    },
-    {
-      id: 2,
-      name: "Oily & Acne skin care",
-      image: image2,
-    },
-    {
-      id: 3,
-      name: "Exclusive skin care",
-      image: image3,
-    },
-    {
-      id: 4,
-      name: "Moisturizer",
-      image: image4,
-    },
-    {
-      id: 5,
-      name: "Premium skinclinic products",
-      image: image5,
-    },
-    {
-      id: 6,
-      name: "Daily skin care",
-      image: image6,
-    },
-    {
-      id: 7,
-      name: "Mature skin care",
-      image: image7,
-    },
-    {
-      id: 8,
-      name: "Hyperpigmentation, Spot & Scar",
-      image: image8,
-    },
-  ];
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
+  // const [categories, setCategories] = useState([]);
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/categories")
+  //     .then((res) => res.json())
+  //     .then((data) => setCategories(data));
+  // }, []);
 
   return (
     <div>
