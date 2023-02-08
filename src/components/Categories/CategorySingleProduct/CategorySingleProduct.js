824e7c46c391f2a8b6f01ed1830824890da2c719
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-02-07 19:32:01 -0800

    edited single category product

--- src/components/Categories/CategorySingleProduct/CategorySingleProduct.js
+++ src/components/Categories/CategorySingleProduct/CategorySingleProduct.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const CategorySingleProduct = () => {
+  const categorySingleProduct = useLoaderData();
+
   return (
     <div>
       <h1>hi</h1>
